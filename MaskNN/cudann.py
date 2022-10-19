847f57e0be9e4ea220d11edbea87de58ea1f0aff
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-19 11:13:38 +0200

    [DOCS]

--- MaskNN/cudann.py
+++ MaskNN/cudann.py
@@ -3,6 +3,8 @@ import cupy as cp
 def nn(mask_1, mask_2, k):
 
     """ For each point in mask_1 the k nearest neighbour belonging to mask_2 are found. 
+    If mask_1 comprises of n_1 non-trivial (i.e. non-zero) points, the output is a numpy array of shape (n_1, k, 2). 
+    That is, for each non-trivial point in mask_1, we find the nearest k points and return their coordinates. 
     The metric used is Manhattan."""
 
     # Extract coordinates of points of mask_1
--- MaskNN/nn.py
+++ MaskNN/nn.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-def nn(mask_1, mask_2, k):
+def nn(mask_1: np.ndarray, mask_2 : np.ndarray, k : int) -> np.ndarray:
 
     """ For each point in mask_1 the k nearest neighbour belonging to mask_2 are found. 
+    If mask_1 comprises of n_1 non-trivial (i.e. non-zero) points, the output is a numpy array of shape (n_1, k, 2). 
+    That is, for each non-trivial point in mask_1, we find the nearest k points and return their coordinates. 
     The metric used is Manhattan."""
 
     # Extract coordinates of points of mask_1
@@ -33,7 +35,7 @@ def nn(mask_1, mask_2, k):
 
 
 
-def nn_value(image, mask_1, mask_2, k):
+def nn_value(image : np.ndarray, mask_1: np.ndarray, mask_2 : np.ndarray, k : int) -> np.ndarray:
 
     """ Retrieve alues of k nearest neighbours """
 
