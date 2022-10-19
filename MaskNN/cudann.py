6e688840544cd2bde091ef47411bc49cef25246a
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-19 11:14:41 +0200

    [BLACK]

--- MaskNN/cudann.py
+++ MaskNN/cudann.py
@@ -1,15 +1,16 @@
 import cupy as cp
 
+
 def nn(mask_1, mask_2, k):
 
-    """ For each point in mask_1 the k nearest neighbour belonging to mask_2 are found. 
-    If mask_1 comprises of n_1 non-trivial (i.e. non-zero) points, the output is a numpy array of shape (n_1, k, 2). 
-    That is, for each non-trivial point in mask_1, we find the nearest k points and return their coordinates. 
+    """For each point in mask_1 the k nearest neighbour belonging to mask_2 are found.
+    If mask_1 comprises of n_1 non-trivial (i.e. non-zero) points, the output is a numpy array of shape (n_1, k, 2).
+    That is, for each non-trivial point in mask_1, we find the nearest k points and return their coordinates.
     The metric used is Manhattan."""
 
     # Extract coordinates of points of mask_1
     mask_1_coordinates = cp.argwhere(mask_1)
-    
+
     # Extract coordinates of points of mask_2
     mask_2_coordinates = cp.argwhere(mask_2)
 
@@ -20,13 +21,13 @@ def nn(mask_1, mask_2, k):
     moduli = cp.abs(difference)
 
     # Sum moduli to obtain the actual Manhattan metric
-    metrics = cp.sum(moduli, axis = 2)
+    metrics = cp.sum(moduli, axis=2)
 
     # Sort points by distance
-    ordered_distances = cp.argsort(metrics, axis = 1)
+    ordered_distances = cp.argsort(metrics, axis=1)
 
     # Keep only the first k neighbours and obtain indices of their coordinates
-    nn_indices = ordered_distances[:, : k]
+    nn_indices = ordered_distances[:, :k]
 
     # Retrieve actual coordinates
     nn_coordinates = mask_2_coordinates[nn_indices]
@@ -34,17 +35,14 @@ def nn(mask_1, mask_2, k):
     return nn_coordinates
 
 
-
 def nn_value(image, mask_1, mask_2, k):
 
-    """ Retrieve alues of k nearest neighbours """
+    """Retrieve alues of k nearest neighbours"""
 
     # Find NN coordinates
     nn_coordinates = nn(mask_1, mask_2, k)
 
     # Retrieve NN values (syntax is convoluted)
-    nn_values = image[tuple(nn_coordinates.T.tolist())].T 
+    nn_values = image[tuple(nn_coordinates.T.tolist())].T
 
     return nn_values
-
-
--- MaskNN/nn.py
+++ MaskNN/nn.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-def nn(mask_1: np.ndarray, mask_2 : np.ndarray, k : int) -> np.ndarray:
 
-    """ For each point in mask_1 the k nearest neighbour belonging to mask_2 are found. 
-    If mask_1 comprises of n_1 non-trivial (i.e. non-zero) points, the output is a numpy array of shape (n_1, k, 2). 
-    That is, for each non-trivial point in mask_1, we find the nearest k points and return their coordinates. 
+def nn(mask_1: np.ndarray, mask_2: np.ndarray, k: int) -> np.ndarray:
+
+    """For each point in mask_1 the k nearest neighbour belonging to mask_2 are found.
+    If mask_1 comprises of n_1 non-trivial (i.e. non-zero) points, the output is a numpy array of shape (n_1, k, 2).
+    That is, for each non-trivial point in mask_1, we find the nearest k points and return their coordinates.
     The metric used is Manhattan."""
 
     # Extract coordinates of points of mask_1
     mask_1_coordinates = np.argwhere(mask_1)
-    
+
     # Extract coordinates of points of mask_2
     mask_2_coordinates = np.argwhere(mask_2)
 
@@ -20,13 +21,13 @@ def nn(mask_1: np.ndarray, mask_2 : np.ndarray, k : int) -> np.ndarray:
     moduli = np.abs(difference)
 
     # Sum moduli to obtain the actual Manhattan metric
-    metrics = np.sum(moduli, axis = 2)
+    metrics = np.sum(moduli, axis=2)
 
     # Sort points by distance
-    ordered_distances = np.argsort(metrics, axis = 1)
+    ordered_distances = np.argsort(metrics, axis=1)
 
     # Keep only the first k neighbours and obtain indices of their coordinates
-    nn_indices = ordered_distances[:, : k]
+    nn_indices = ordered_distances[:, :k]
 
     # Retrieve actual coordinates
     nn_coordinates = mask_2_coordinates[nn_indices]
@@ -34,17 +35,16 @@ def nn(mask_1: np.ndarray, mask_2 : np.ndarray, k : int) -> np.ndarray:
     return nn_coordinates
 
 
+def nn_value(
+    image: np.ndarray, mask_1: np.ndarray, mask_2: np.ndarray, k: int
+) -> np.ndarray:
 
-def nn_value(image : np.ndarray, mask_1: np.ndarray, mask_2 : np.ndarray, k : int) -> np.ndarray:
-
-    """ Retrieve alues of k nearest neighbours """
+    """Retrieve alues of k nearest neighbours"""
 
     # Find NN coordinates
     nn_coordinates = nn(mask_1, mask_2, k)
 
     # Retrieve NN values (syntax is convoluted)
-    nn_values = image[tuple(nn_coordinates.T.tolist())].T 
+    nn_values = image[tuple(nn_coordinates.T.tolist())].T
 
     return nn_values
-
-
